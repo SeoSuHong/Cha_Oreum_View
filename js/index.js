cfa19613e8046a9d4acbeac5e7a4d379f7f6a4e7
SeoSuHong <tbgkdntm@naver.com>
2022-10-07 20:50:46 +0900

    [add] main 구현 중

--- js/index.js
+++ js/index.js
@@ -24,8 +24,6 @@ $(function() {
         let sub_cnt = $(sub).children().length;
         const sub_height = 42;
 
-        console.log(sub_height);
-
         if($(sub).css('height') === '0px') {
             $(sub).animate({height:sub_cnt*sub_height+"px"}, 300);
         } else {
@@ -33,3 +31,59 @@ $(function() {
         }
     })
 })
+
+// 카테고리 클릭 시 parameter 저장 후 페이지 이동
+$(function() {
+    let categories = $('.subCategory');
+    let hideCategory = $('#category');
+
+    $(categories).click(function() {
+        let name = $(this).children('p').text();
+
+        hideCategory.val(name);
+        location.href = 'index.html?c=' + name;
+    })
+})
+
+
+// main
+// 필터 검색 기능
+function search() {
+    let field = $('#field').val();
+    let keyword = $('#keyword').val();
+
+    if(keyword == '') {
+        alert('검색 키워드를 입력해 주세요.');
+    } else {
+        if(field === 'title') {
+            $('#title').val(keyword);
+            $('#nickname').val('');
+        } else {
+            $('#nickname').val(keyword);
+            $('#title').val('');
+        }
+        submit();
+    }
+}
+
+// 정렬 기능
+function sort(s) {
+    $('#sort').val(s);
+    submit();
+}
+
+// 컨텐츠
+
+
+// submit
+function submit(page) {
+    let category = $('#category').val();
+    let title = $('#title').val();
+    let nickname = $('#nickname').val();
+    let sort = $('#sort').val();
+
+    let url = 'index.html?c=' + category + '&t=' + title + '&n=' + nickname + '&s=' + sort + '&p=' + page;
+
+    indexForm.action = url;
+    indexForm.submit();
+}
