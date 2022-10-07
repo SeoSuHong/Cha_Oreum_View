219d4a5379a05efbb6500438f6b87d2b927a187f
SeoSuHong <tbgkdntm@naver.com>
2022-10-08 02:40:06 +0900

    [update] index페이지 submit 수정 [add] detail페이지 추가 및 header, aside, footer 추가

--- js/detail.js
+++ js/detail.js
@@ -47,37 +47,4 @@ $(function() {
 
 
 // main
-// 필터 검색 기능
-function search() {
-    let field = $('#field').val();
-    let keyword = $('#keyword').val();
 
-    if(keyword == '') {
-        alert('검색 키워드를 입력해 주세요.');
-    } else {
-        if(field === 'title') {
-            $('#title').val(keyword);
-            $('#nickname').val('');
-        } else {
-            $('#nickname').val(keyword);
-            $('#title').val('');
-        }
-        indexForm.submit();
-    }
-}
-
-// 정렬 기능
-function sort(s) {
-    $('#sort').val(s);
-    indexForm.submit();
-}
-
-// 페이지 클릭 시
-$(function() {
-    $('.page').click(function() {
-        let page = $(this).text();
-        $('#page').val(page);
-
-        indexForm.submit();
-    })
-})
