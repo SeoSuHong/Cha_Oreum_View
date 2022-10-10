55ae1cdf408f5f633a411f01c116082374c58d2c
SeoSuHong <tbgkdntm@naver.com>
2022-10-10 15:05:28 +0900

    [add] detail페이지 첨부파일 추가 [update]댓글, 답글 디자인 업데이트 detail페이지 디자인 구현 완료

--- js/detail.js
+++ js/detail.js
@@ -47,4 +47,19 @@ $(function() {
 
 
 // main
+// id="file"의 line-height 설정
+$(function() {
+    const file_height = $('.fileName').height();
+    let file_count = $('.fileName').length;
 
+    $('#file').css({"line-height":file_height*file_count+'px'});
+})
+
+// 답글 쓰기 버튼 클릭 시
+$(function() {
+    let reply_btn = $('.reply_btn');
+
+    $(reply_btn).click(function() {
+        $($(this).parent().parent().next()).show();
+    })
+})
