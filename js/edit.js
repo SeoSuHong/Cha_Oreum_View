6c9dc16945588043c06238fc682fda1fed09c3f8
SeoSuHong <tbgkdntm@naver.com>
2022-10-11 03:32:08 +0900

    [update] Cha Oreum view 완료

--- js/edit.js
+++ js/edit.js
@@ -45,26 +45,20 @@ $(function() {
     })
 })
 
-
 // main
-// id="file"의 line-height 설정
-$(function() {
-    const file_height = $('.fileName').height();
-    let file_count = $('.fileName').length;
-
-    $('#file').css({"line-height":file_height*file_count+'px'});
-})
-
-// 댓글 쓰기 버튼 클릭 시
-function comment_focus() {
-    $('#comment_txt').focus();
+// 파일 삭제 클릭 시
+function file_delete(obj) {
+    let del = $(obj).parent();
+    $(del).remove();
 }
 
-// 답글 쓰기 버튼 클릭 시
-$(function() {
-    let reply_btn = $('.reply_btn');
-
-    $(reply_btn).click(function() {
-        $($(this).parent().parent().next()).show();
-    })
-})
+// summernote 에디터
+$(document).ready(function() {
+	$('#contents').summernote({
+		  height: 500,
+		  minHeight: null,
+		  maxHeight: null,
+		  lang: "ko-KR",
+		  placeholder: '내용을 입력해 주세요.'
+	});
+});
--- js/reg.js
+++ js/reg.js
@@ -47,15 +47,13 @@ $(function() {
 
 
 // main
-// 내용 부분 summernote 에디터 적용
+// summernote 에디터
 $(document).ready(function() {
-	//여기 아래 부분
 	$('#contents').summernote({
-		  height: 500,                 // 에디터 높이
-		  minHeight: null,             // 최소 높이
-		  maxHeight: null,             // 최대 높이
-		  lang: "ko-KR",					// 한글 설정
-		  placeholder: '최대 2048자까지 쓸 수 있습니다'	//placeholder 설정
-          
+		  height: 500,
+		  minHeight: null,
+		  maxHeight: null,
+		  lang: "ko-KR",
+		  placeholder: '내용을 입력해 주세요.'
 	});
 });
